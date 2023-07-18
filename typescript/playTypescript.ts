10fca67694422845064eebe598e56b4fed38a8a2
Samuel Matos <samuhmatos@gmail.com>
2023-07-18 09:52:06 -0300

    TypeScript do zero: Manipulação de Tipos: Indexed Access Types

--- typescript/playTypescript.ts
+++ typescript/playTypescript.ts
@@ -1,14 +1,22 @@
-import { Post, User } from "./interfaces";import { coffstack, comment, vitor } from "./mocksData";
+import { Post, PostComment, User } from "./interfaces";
+import { coffstack, vitor } from "./mocksData";
 
 export const lucas: User = {
   name: "Lucas",
   username: "lucas123",
   email: "lucas123@gmail.com",
 };
+export const comment: PostComment = {
+  content: "Conteudo do comentario",
+  likes: 5,
+  user: vitor,
+};
 
-function getProperty<Type>(value: Type, key: keyof Type) {
+// type ContentType = PostComment["user"]
+
+function getProperty<Type, key extends keyof Type>(value: Type, key: key) {
   return value[key];
 }
 
-const value = getProperty(comment, "content");
+const value = getProperty(comment, "user");
 console.log(value);
