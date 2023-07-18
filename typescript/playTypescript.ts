ebd998fb61ec3bdecb5876e7fbb3bc0dbf97ed3a
Samuel Matos <samuhmatos@gmail.com>
2023-07-18 09:20:50 -0300

    TypeScript do zero: Tipos primitivos e Compile Options

--- typescript/playTypescript.ts
+++ typescript/playTypescript.ts
@@ -1,11 +1,20 @@
-// Tipagem estática
-function flipCoin() {
-  return Math.random() < 0.5 ? "cara" : "coroa";
-}
-const result = flipCoin();
+const message: string = "Hello world";const numberValue: number = 10;
+const booleanValue: boolean = false;
 
-if (result === "cara") {
-  console.log("Você ganhou!");
-} else {
-  console.log("Você perdeu!");
-}
+const nullValue: null = null;
+const undefinedValue: undefined = undefined;
+
+const value: "primary" | "secondary" = "primary";
+
+const list: string[] = ["a", "b", "c"];
+
+const users = [
+  { name: "Jack", age: 20 },
+  { name: "Jane", age: 21 },
+];
+
+//const jack = users.find((user) => user.name === "Jack");
+//console.log(jack);
+
+const jane = users.length > 4 ? users[3] : null;
+console.log(jane?.age);
